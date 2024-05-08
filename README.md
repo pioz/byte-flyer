5d776839b4849c9cb28c7bd0ea09936edbc52254
pioz <epilotto@gmx.com>
2024-05-08 22:37:20 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# byte-flyer
+
+Send a byte via UDP
+
+## Install
+
+```
+npm install byte-flyer
+```
+
+## Usage
+
+```
+byte-flyer -i 192.168.0.100 -p 1234 -b 0x2C
+```
---
+++ main.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const dgram = require('dgram')
+const client = dgram.createSocket('udp4')
+const { Command } = require('commander')
+
+const hexStringToByte = (hexString) => {
+  if (hexString.startsWith('0x')) {
+    hexString = hexString.slice(2)
+  }
+  return parseInt(hexString, 16)
+}
+
+const sendByte = async (ip, port, byte) => {
+  return new Promise((resolve, reject) => {
+    const data = Buffer.from([byte])
+    client.send(data, port, ip, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+// Start here
+
+const options = new Command()
+  .name('byte-flyer')
+  .description('Send a byte via UDP')
+  .version('0.1.0')
+  .requiredOption('-i, --ip <char>', 'IP address')
+  .option('-p, --port <int>', 'port', 1234)
+  .requiredOption('-b, --byte <char>', 'byte to send (HEX)')
+  .parse()
+  .opts();
+
+(async () => {
+  await sendByte(options.ip, options.port, hexStringToByte(options.byte))
+  client.close()
+})()
